a6e5a20c925ff705520ffc7c4ca9e8f730efdcf7
PrabeshPP <prabesh.bista369@gmail.com>
2022-10-19 00:52:28 -0400

    [completed] worked on api

--- pages/api/upload.ts
+++ pages/api/upload.ts
@@ -1,7 +1,25 @@
 import { PrismaClient } from "@prisma/client";
+import type { NextApiRequest, NextApiResponse } from "next";
 
 
- const upload=()=>{
+const upload = async (req: NextApiRequest, res: NextApiResponse) => {
+    const { name, upload } = req.body;
+    const prisma = new PrismaClient();
+    try {
+        const result = await prisma.upload.create({
+            data: {
+                name,
+                upload
+            }
+        });
+
+        res.status(200).json(result);
+
+    } catch (err) {
+        console.log(err);
+        res.json(err);
+    }
+
 
 }
 
