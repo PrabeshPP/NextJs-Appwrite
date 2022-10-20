3c3daee0b1939603abe1b45132894f05543334b2
PrabeshPP <prabesh.bista369@gmail.com>
2022-10-19 21:35:09 -0400

    [initialized] session

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,37 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
+import React, { useState,useEffect } from 'react';
+import { appwriteSDK,accountSDK } from '../helper/util';
 
 const Home: NextPage = () => {
+  const [file,setFile]=useState<File>();
+  const [name,setName]=useState<String>("");
+
+  const checkSession=async()=>{
+    const validSession=accountSDK().get();
+    if(!validSession){
+     try{
+      const response= await accountSDK().createAnonymousSession();
+      console.log(response);
+     }catch(err){
+        console.log(err);
+     }
+    }
+  }
+
+
+  useEffect(()=>{
+      
+  },[])
+
+
+  const handleSubmit=async(event:any)=>{
+        event.preventDefault();
+        console.log(event.target);
+  }
+
   return (
     <div className={styles.container}>
       <Head>
@@ -14,15 +42,16 @@ const Home: NextPage = () => {
       <main className={styles.box}>
         <div className={styles.uploadBox}>
             <div className={styles.titleBox}>Testing Multiple DBs</div>
-            <form className={styles.formBox}>
+            <form className={styles.formBox} onSubmit={handleSubmit}>
               <div className={styles.nameBox}>
               <label>Name</label>
-              <input type="text"/>
+              <input type="text" name='namde'/>
               </div>
               <div className={styles.uploadBox1}>
               <label>Select image:</label>
-              <input type="file" className={styles.fileUpload} accept="image/*"/>
+              <input name="fileName"type="file" className={styles.fileUpload} accept="image/*"/>
               </div>
+              <button className={styles.submitButton}>Submit</button>
             </form>
         </div>
       </main>
