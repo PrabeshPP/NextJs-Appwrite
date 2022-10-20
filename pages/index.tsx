d01f83c56280b45a65da9f7e902c8ac8a122ccec
PrabeshPP <prabesh.bista369@gmail.com>
2022-10-20 00:14:41 -0400

    [completed] file upload

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,32 +4,50 @@ import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 import React, { useState,useEffect } from 'react';
 import { appwriteSDK,accountSDK } from '../helper/util';
+import { url } from 'inspector';
 
 const Home: NextPage = () => {
-  const [file,setFile]=useState<File>();
+  const [file,setFile]=useState<File>(null!);
   const [name,setName]=useState<String>("");
 
   const checkSession=async()=>{
-    const validSession=accountSDK().get();
+    const validSession= accountSDK().get();
     if(!validSession){
-     try{
-      const response= await accountSDK().createAnonymousSession();
-      console.log(response);
-     }catch(err){
-        console.log(err);
-     }
+      try{
+        const promise= await accountSDK().createAnonymousSession();
+        console.log(promise);
+      }catch(err){
+          console.log(err);
+      }
+      
     }
+    
+
   }
 
 
   useEffect(()=>{
-      
+      checkSession();
   },[])
 
 
   const handleSubmit=async(event:any)=>{
         event.preventDefault();
-        console.log(event.target);
+        const sdk=await appwriteSDK().createFile(
+          '634f471be7eeae64119f',
+          'unique()',
+          file
+        )
+
+        const data={name,upload:sdk.$id};
+        const response=await  fetch("api/upload",{
+          method:"POST",
+          headers:{'Content-Type':"application/json"},
+          body:JSON.stringify(data)
+        });
+        alert("File Uploaded Successfully!");
+
+        
   }
 
   return (
@@ -45,11 +63,15 @@ const Home: NextPage = () => {
             <form className={styles.formBox} onSubmit={handleSubmit}>
               <div className={styles.nameBox}>
               <label>Name</label>
-              <input type="text" name='namde'/>
+              <input type="text" name='namde' onChange={(event)=>{
+                  setName(event.target.value);
+              }}/>
               </div>
               <div className={styles.uploadBox1}>
               <label>Select image:</label>
-              <input name="fileName"type="file" className={styles.fileUpload} accept="image/*"/>
+              <input name="fileName"type="file" className={styles.fileUpload} accept="image/*" onChange={(event)=>{
+                  setFile(event.target.files![0]);
+              }}/>
               </div>
               <button className={styles.submitButton}>Submit</button>
             </form>
